5a7bfe74f349d7ec53dc3401b967d758cd07faed
mrmayman <navneetkrishna22@gmail.com>
2025-08-26 22:13:35 +0530

    Dev: Print Done after importing modpack, Add (unused) GameExpectation struct

--- crates/ql_mod_manager/src/store/error.rs
+++ crates/ql_mod_manager/src/store/error.rs
@@ -1,4 +1,4 @@
-use std::num::ParseIntError;
+use std::{fmt::Display, num::ParseIntError};
 
 use ql_core::{impl_3_errs_jri, IoError, JsonError, RequestError};
 use thiserror::Error;
@@ -38,3 +38,28 @@ pub enum ModError {
 }
 
 impl_3_errs_jri!(ModError, Json, RequestError, Io);
+
+#[derive(Debug)]
+pub struct GameExpectation {
+    pub expected: String,
+    pub got: String,
+}
+
+impl Display for GameExpectation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.got == "Vanilla" {
+            write!(
+                f,
+                "You don't have {exp} installed!\nPlease install {exp}",
+                exp = self.expected
+            )
+        } else {
+            write!(
+                f,
+                "Expected {expected}, got {got}\nPlease install {expected}",
+                expected = self.expected,
+                got = self.got
+            )
+        }
+    }
+}
